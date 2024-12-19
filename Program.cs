1738cf3aa5dbab9b8bb206db58173cecbfac1bbb
jamesleu923 <jamesleu923@gmail.com>
2024-12-19 11:59:39 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20241219_OOP001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //========================================
+            //單一物件型態的宣告, 宣告時即會配Memory.
+            //=======================================
+            Users LabUser = new Users();
+            //宣告完即可操作物件:LabUser
+            LabUser.Id = 1;
+            
+            //===========================================
+            //陣列型態的物件宣告
+            //====================================
+            Users[] PcUsers = new Users[50];            
+            //若用陣列型態宣告物件後,還要配一次記憶體空間
+            for (int i = 0; i < PcUsers.Length; i++)
+            {
+                PcUsers[i] = new Users(i+1, "PcUsers" + (i + 1).ToString());
+            }
+
+
+            for (int i = 0; i < 50; i++) {
+               //   // Create a new Users object
+               // PcUsers[i]._Id = i + 1;
+                //PcUsers[i].Name="PcUsers"+(i+1).ToString();
+                Console.Write("ID is :"+ PcUsers[i].Id.ToString());
+                Console.WriteLine(" Name is :"+ PcUsers[i].Name.ToString()); 
+            }
+
+        }
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Users
+{
+    public Users()
+    {
+        //預設建構子函式,會hide起來...除非你打出來.
+        //但...在Java中,一定要打出來.
+    }
+    public Users(int Myid, string Myname)
+    {
+        Id = Myid;
+        Name = Myname;
+    }
+    /*屬性宣告要加底線,設為private模式 */
+    /*要存取屬性,要經由存取子函式*/
+    /*目的:符合物件導向之物件封裝的特性 */
+    private int _Id;
+    private string _Name;
+    /*
+     * //存取子函式
+     * 特色: 做為屬性的存取與設定之用.
+     * 名字: 一般會跟屬性的名字一樣,但是屬性的宣告會加一個底線:_,
+     * 而存取子函式把底線去除
+     */
+    public int Id //存取子函式
+    {
+        get { return _Id; }
+        set { _Id = value; }
+    }
+    public string Name
+    {
+        get { return _Name; }
+        set { _Name = value; }
+    }
+
+}
+
+
+/*
+ 版本一 不封裝,都設public 
+public class Users
+{
+    public Users()
+    {
+        //預設建構子函式,會hide起來...除非你打出來.
+        //但...在Java中,一定要打出來.
+    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
+/*
+namespace _20241219_OOP001
+{
+    internal class Users
+    {
+    }
+}
+*/
